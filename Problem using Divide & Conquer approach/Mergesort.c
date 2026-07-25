c4cd3c16228e8429b5ad7bb0b1516e1e93079871
Kedar Pinniboyina <84981728+kedarpinniboyina@users.noreply.github.com>
2026-07-25 11:53:56 +0530

    Add files via upload

---
+++ Problem using Divide & Conquer approach/Mergesort.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void merge(int a[], int low, int mid, int high)
+{
+    int i = low, j = mid + 1, k = low;
+    int temp[100];
+
+    while (i <= mid && j <= high)
+    {
+        if (a[i] <= a[j]){
+            temp[k] = a[i];
+            i++;
+            
+        }
+        else{
+            temp[k] = a[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i <= mid){
+        temp[k] = a[i];
+        i++;
+        k++;
+    }
+
+    while (j <= high){
+        temp[k]=a[j];
+        j++;
+        k++;
+    }
+    for (i = low; i <= high; i++)
+        a[i] = temp[i];
+}
+
+void mergeSort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+
+        merge(a, low, mid, high);
+    }
+}
+
+int main()
+{
+    int n, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+
+    printf("Enter elements:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    mergeSort(a, 0, n - 1);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
