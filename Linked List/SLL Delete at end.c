ccafe945014bb897302ebb88e22f5dd9e1be3930
Kedar Pinniboyina <84981728+kedarpinniboyina@users.noreply.github.com>
2026-08-18 23:18:28 +0530

    Create SLL Delete at end.c

---
+++ Linked List/SLL Delete at end.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+int main()
+{
+    struct Node *head = NULL;
+    struct Node *newNode;
+    struct Node *temp;
+
+    int n, i;
+
+    // Read number of nodes
+    printf("Enter the No of Nodes: ");
+    scanf("%d", &n);
+
+    // Create linked list
+    for(i = 1; i <= n; i++)
+    {
+        // Create new node
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        // Read data
+        printf("Enter the Data: ");
+        scanf("%d", &newNode->data);
+
+        // Insert at beginning
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Display before deletion
+    printf("\nBefore Deletion: ");
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    // Delete at end
+    if(head == NULL)
+    {
+        printf("\nList is Empty");
+    }
+    else if(head->next == NULL)
+    {
+        // Only one node
+        free(head);
+        head = NULL;
+    }
+    else
+    {
+        // Move to second-last node
+        temp = head;
+
+        while(temp->next->next != NULL)
+        {
+            temp = temp->next;
+        }
+
+        // Delete last node
+        free(temp->next);
+
+        // Second-last node becomes last node
+        temp->next = NULL;
+    }
+
+    // Display after deletion
+    printf("\nAfter Deletion: ");
+
+    temp = head;
+
+    while(temp != NULL)
+    {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
