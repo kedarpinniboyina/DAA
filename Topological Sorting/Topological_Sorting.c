ada35a09952f837dd5f9660fa467085c665b3357
Kedar Pinniboyina <84981728+kedarpinniboyina@users.noreply.github.com>
2026-09-10 11:01:16 +0530

    Update Topological_Sorting.c

--- Topological Sorting/Topological_Sorting.c
+++ Topological Sorting/Topological_Sorting.c
@@ -1,55 +1,48 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i, j, count = 0;
-    int graph[10][10], indegree[10] = {0};
-    int queue[10], front = 0, rear = 0;
+int main() {
+    int n, i, j, top = -1;
+    int graph[10][10], visited[10] = {0};
+    int stack[10];
 
     printf("Enter number of vertices: ");
     scanf("%d", &n);
 
     printf("Enter adjacency matrix:\n");
-    for(i = 0; i < n; i++)
-    {
-        for(j = 0; j < n; j++)
-        {
-            scanf("%d", 
-            &graph[i][j]);
-
-            if(graph[i][j] == 1)
-                indegree[j]++;
+    for(i = 0; i < n; i++)  {
+        for(j = 0; j < n; j++)   {
+            scanf("%d", &graph[i][j]);
         }
     }
-    // Add vertices with indegree 0 to queue
-    for(i = 0; i < n; i++)
+// DFS function using recursion
+    void DFS(int v)
     {
-        if(indegree[i] == 0)
-            queue[rear++] = i;
+        int j;
+        visited[v] = 1;
+
+        // Visit all connected vertices
+        for(j = 0; j < n; j++) {
+            if(graph[v][j] == 1 && visited[j] == 0) {
+                DFS(j);
+            }
+        }
+
+        // Add vertex to stack after visiting all neighbours
+        stack[++top] = v;
+    }
+ // Perform DFS for all vertices
+    for(i = 0; i < n; i++) {
+        if(visited[i] == 0) {
+            DFS(i);
+        }
     }
 
     printf("Topological Sort: ");
-    // Process vertices in queue
-    while(front < rear)
-    {
-        // Remove a vertex from queue
-        int v = queue[front++];
-        printf("%d ", v);
-        count++;
-        // Check all vertices connected from v
-        for(j = 0; j < n; j++)
-        {
-            if(graph[v][j] == 1)
-            {
-                // Remove the edge by decreasing indegree
-                indegree[j]--;
-                
-                // If indegree becomes 0, add vertex to queue
-                if(indegree[j] == 0)
-                    queue[rear++] = j;
-            }
-        }
+
+    // Print stack in reverse order
+    while(top >= 0)  {
+        printf("%d ", stack[top--]);
     }
-    
+
     return 0;
 }
